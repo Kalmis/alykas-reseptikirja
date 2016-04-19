796f1b2a920dcbb1b155e155aed955db9e738a91
Kalmis <Kalmis@ipro.lan>
2016-04-19 15:10:40 +0300

    Päävalikon rakenteen muokkaamista sekä varastotilanne menun implementointi

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class Main():
+class Main:
     
     def __init__(self):
         
@@ -15,12 +15,15 @@ class Main():
             print("2. Raaka-aineet")
             print("3. Reseptit")
             print("4. Haku")
+            print("5. Tallenna")
+            print("6. Lataa")
             print("0. Sulje ohjelma")
             
             # Pyydetään käyttäjältä inputtia, kunnes saadaan validi vastaus. 
             # TÄmän jälkeen break, jotta tulostetaan menu uudelleen
             while 1:
                 valinta = input("Valintasi > ")
+                print("\n")
                 if valinta == "1":
                     self.StorageMenu()
                     break;
@@ -33,6 +36,12 @@ class Main():
                 elif valinta == "4":
                     self.SearchMenu()
                     break;
+                elif valinta == "5":
+                    self.SaveMenu()
+                    break
+                elif valinta == "6":
+                    self.LoadMenu()
+                    break
                 elif valinta == "0":
                     return(0)
                 else:
@@ -40,8 +49,29 @@ class Main():
                 
         
         
+        
+        
     def StorageMenu(self):
-        print("STorageMenu")
+        while 1:
+            print("1. Kerro lisätietoja raaka-aineesta")
+            print("2. Näytä varastotilanne")
+            print("0. Takaisin")
+            
+            # Pyydetään käyttäjältä inputtia, kunnes saadaan validi vastaus. 
+            # TÄmän jälkeen break, jotta tulostetaan menu uudelleen
+            while 1:
+                valinta = input("Valintasi > ")
+                print("\n")
+                if valinta == "1":
+                    print("Lisätietoja")
+                    break;
+                elif valinta == "2":
+                    print("Varastotilanne")
+                    break;
+                elif valinta == "0":
+                    return(0)
+                else:
+                    print("Virheellinen valinta.")   
         
     def IngredientsMenu(self):
         print("IngredientsMenu")
@@ -51,6 +81,12 @@ class Main():
         
     def SearchMenu(self):
         print("SearchMenu")    
+        
+    def SaveMenu(self):
+        print("SaveMenu")
+    
+    def LoadMenu(self):
+        print("LoadMenu")        
         
             
 program = Main()
