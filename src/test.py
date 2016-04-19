1d5c8651f7def0e07e338d6f6b755190a14a0a2b
Kalmis <Kalmis@ipro.lan>
2016-04-19 22:04:55 +0300

    Lisää loadIngredients testejä.

--- src/test.py
+++ src/test.py
@@ -45,18 +45,44 @@ class Test(unittest.TestCase):
         self.input_file.write('\nAllergen     : Laktoosi\n')
         self.input_file.write('\nAllergen     : Maissi\n')
         
+        self.input_file.write('#Ingredient\n')
+        self.input_file.write('Date             : 18.4.2015\n')
+        self.input_file.write('Name             : Kala2\n')
+        self.input_file.write('\nDensity            : asd\n')
+        self.input_file.write('\nQuantity            : 2 \n')
+        self.input_file.write('\nUnit     : kg\n')
+        self.input_file.write('\nRecipe     : \n')
+        self.input_file.write('\nAllergen     : Laktoosi\n')
+        self.input_file.write('\nAllergen     : Maissi\n')
+        
+        self.input_file.write('#Ingredient\n')
+        self.input_file.write('Date             : 18.4.2015\n')
+        self.input_file.write('Name             : Kala3\n')
+        self.input_file.write('\nDensity            : 1\n')
+        self.input_file.write('\nQuantity            : kasd \n')
+        self.input_file.write('\nUnit     : kg\n')
+        self.input_file.write('\nRecipe     : \n')
+        self.input_file.write('\nAllergen     : Laktoosi\n')
+        self.input_file.write('\nAllergen     : Maissi\n')
         self.input_file.seek(0, 0) 
 
-        recipesList = self.IO.loadIngredients(self.input_file)
+        recipesList,succesCount,errorCount = self.IO.loadIngredients(self.input_file)
         self.input_file.close()
-        ingredient = recipesList[0]
+        self.assertEqual(1,succesCount,"Väärä määrä onnistuneita lukuja")
+        self.assertEqual(2, errorCount, "Väärä määrä epäonnistuneita lukuja")
+        self.assertEqual(succesCount, len(recipesList), "Lista eripituinen kuin onnistuneet lukemiset")
+        if len(recipesList) > 0:
+            ingredient = recipesList[0]
+            self.assertEqual("18.4.2015", ingredient.getDate(), "Raaka-aineen päivä ei täsmää")
+            self.assertEqual("Kala", ingredient.getName(), "Raaka-aineen nimi ei täsmää")
+            self.assertEqual(1, ingredient.getDensity(), "Raaka-aineen tiheys ei täsmää")
+            self.assertEqual(2, ingredient.getQuantity(), "Raaka-aineen määrä ei täsmää")
+            self.assertEqual("kg", ingredient.getUnit(), "Raaka-aineen yksikkö ei täsmää")
+            self.assertEqual(["Laktoosi","Maissi"], ingredient.getAllergens(), "Raaka-aineen allergeenit eivät täsmää")
+
         
-        self.assertEqual("18.4.2015", ingredient.getDate(), "Raaka-aineen päivä ei täsmää")
-        self.assertEqual("Kala", ingredient.getName(), "Raaka-aineen nimi ei täsmää")
-        self.assertEqual(1, ingredient.getDensity(), "Raaka-aineen tiheys ei täsmää")
-        self.assertEqual(2, ingredient.getQuantity(), "Raaka-aineen määrä ei täsmää")
-        self.assertEqual("kg", ingredient.getUnit(), "Raaka-aineen yksikkö ei täsmää")
-        self.assertEqual(["Laktoosi","Maissi"], ingredient.getAllergens(), "Raaka-aineen allergeenit eivät täsmää")
+        
+        
         
         
 #===============================================================================
