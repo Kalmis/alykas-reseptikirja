092ee230f76b693b2f942c4b5994c5e9cdbb16a4
Kalmis <Kalmis@ipro.lan>
2016-04-19 18:21:35 +0300

    Error tyypin lisäys

--- src/corrupted_file_errors.py
+++ src/corrupted_file_errors.py
@@ -13,3 +13,8 @@ class CorruptedRecipeFileError(Exception):
 
     def __init__(self, message):
         super(CorruptedRecipeFileError, self).__init__(message)
+        
+class CorruptedStorageFileError(Exception):
+
+    def __init__(self, message):
+        super(CorruptedStorageFileError, self).__init__(message)
